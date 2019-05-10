5dd8a9a6cc7e5e09897304a2d858f5e48f012f07
gifton <giftono@gmail.com>
2019-05-10 02:07:27 -0700

    created MSGBoardLinkView

---
+++ Rethought/Rethought/View/Auxiliary/MSG/MessageBoard/components/entry+thought/MSGBoardLinkView.swift
@@ -0,0 +1,22 @@
+
+import Foundation
+import UIKit
+
+class MSGBoardLinkView: UIView {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    private func setViews() {
+        
+    }
+    
+    private func styleViews() {
+        
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
