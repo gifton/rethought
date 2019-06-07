14bb02ac4ad747992f63b69096f359699bd08825
gifton <giftono@gmail.com>
2019-06-07 15:29:58 -0500

    added new assets, entryactionBar updates

--- Rethought/Rethought/View/Auxiliary/EntryActionBar.swift
+++ Rethought/Rethought/View/Auxiliary/EntryActionBar.swift
@@ -24,7 +24,31 @@ class EntryActionBar: UIView {
     
     // MARK: private objects
     private var iconLabel = UILabel()
-    private var closeBtn = UIButton()
     private var infoView = UIView()
-    private var deleteButton = UIButton()
+    
+    // MARK: public objects
+    public var closeButton = UIButton()
+    public var deleteButton = UIButton()
+    
+    private func setViews() {
+        // style views
+        closeButton.layer.cornerRadius = 55.0/2
+        closeButton.setImage(#imageLiteral(resourceName: "close-1"), for: .normal)
+        iconLabel.font = .boldSystemFont(ofSize: Device.fontSize.emojiLG.rawValue)
+        iconLabel.text = icon.icon
+        infoView.backgroundColor = Device.colors.blue
+        infoView.layer.cornerRadius = 65.0 / 2
+        
+        // add frames
+        infoView.frame = CGRect(x: 0, y: 0, width: frame.width * 0.556, height: 65)
+        closeButton.frame = CGRect(x: infoView.frame.width - 60, y: 5, width: 55, height: 55)
+        iconLabel.frame = CGRect(x: 10, y: 5, width: 55, height: 55)
+        deleteButton.frame = CGRect(x: ((frame.width - infoView.frame.width) - 60) / 2, y: 20, width: 60, height: 25)
+        
+        // add views
+        addSubview(infoView)
+        addSubview(deleteButton)
+        infoView.addSubview(iconLabel)
+        infoView.addSubview(closeButton)
+    }
 }
