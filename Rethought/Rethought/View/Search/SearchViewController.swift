7d9c93ab06d31eb2cbc8b7d73f877cc56fb1d994
gifton <giftono@gmail.com>
2019-06-18 10:56:55 -0700

    added search cell files

--- Rethought/Rethought/View/Search/SearchViewController.swift
+++ Rethought/Rethought/View/Search/SearchViewController.swift
@@ -6,5 +6,32 @@ class SearchViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = Device.colors.offWhite
+        setView()
+    }
+    
+    let tv: UITableView = {
+        let tv = UITableView()
+        
+        return tv
+    }()
+    
+    func setView() {
+        view.addSubview(tv)
+        tv.frame = view.frame
+        tv.delegate = self
+        tv.dataSource = self
+        
+    }
+}
+
+extension SearchViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 25
+    }
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 125
     }
 }
